f8a1894e1894ff78ace6432534b89caa6e80c8c0
Your Name <you@example.com>
2022-09-13 17:12:07 -0400

    updated portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
+var countDownDate = new Date("January 11, 2023 12:00:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("countdown").innerHTML = `${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds `;
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "The 2022-2023 Has Officially Begun!";
+  }
+}, 1000);
+
 $(".logo-slider").slick({
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
+    mobileFirst: true,
+    adaptiveHeight: true,
     responsive: [
       {
         breakpoint: 1024,
@@ -29,4 +46,14 @@ $(".logo-slider").slick({
         }
       }
     ]
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+      const $notification = $delete.parentNode;
+
+      $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification);
+      });
+    });
   });
